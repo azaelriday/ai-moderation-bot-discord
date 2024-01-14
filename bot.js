4081b9bd757dbb9215c18f7b0f95d294d8a7652f
azaelriday <46460104+azaelriday@users.noreply.github.com>
2024-01-14 13:17:24 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,12 +6,12 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content.includes('lemon')) {
-        msg.delete()
-            .then(deletedMsg => console.log(`Deleted message from ${deletedMsg.author.username}`))
-            .catch(error => console.error('Could not delete message due to:', error));
+client.on('message', message => {
+    if (message.content.includes('lemon')) {
+        message.delete()
+            .then(msg => console.log(`Deleted message from ${msg.author.username} containing the word 'lemon'`))
+            .catch(console.error);
     }
 });
 
-client.login('MTE5NTkwMTg2MjUzNzM0MzA2Nw.GhFeX1.AefYRGuXSbhADsX_QoDUozi8Hdsd5MGBoHIXY4');
+client.login(process.env.DISCORD_BOT_TOKEN);
